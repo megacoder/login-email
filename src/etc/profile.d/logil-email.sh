f86383e7b335fe8c568b98f952d93cce5472dad3
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-10-29 11:16:38 -0500

    Added script and config file.

---
+++ src/etc/profile.d/logil-email.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+MAILTO=root
+
+CONFIG=/etc/login-email.conf
+
+declare -A watchfor
+
+watchfor['root']=yes
+
+if [ -f "${CONFIG}" ]; then
+	/bin/sed -r -e 's/[#].*$//' -e '/^[[:space:]]*$/d' "${CONFIG}" |
+	while read name; do
+		watchfor["${name}"]=yes
+	done
+fi
+
+if [ "${watchfor[${USER}]}" != '' ]; then
+	(
+		FMT='%7s:  %s\n'
+		/bin/printf "${FMT}" 'Date' "$(/bin/date)"
+		/bin/printf "${FMT}" 'User' "${USER}"
+		/bin/printf "${FMT}" 'Host' "${HOSTNAME}"
+		/bin/printf "${FMT}" 'Shell' "${SHELL}"
+		/bin/printf "${FMT}" 'TTY' "$(/bin/tty)"
+
+		cat <<-EOF
+
+		Last few logins:
+		-------------------------------------------------------
+		$(
+			/bin/last ${USER} | /bin/head -n5
+		)
+		-------------------------------------------------------
+		EOF
+	) | /bin/mailx -s "Login watch for ${HOSTNAME}" "${MAILTO}"
+fi
