e4545a35eb886b9c187345a09f3fede52134df9d
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-10-29 20:13:19 -0500

    Go ahead and build the RPM if the user is set up for it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,8 @@
 markdown2 README.md | tee README.html | lynx -dump -stdin >README
 autoreconf -fvim
 ./configure
-make -j20 dist
+make dist
+if [ -d ${HOME}/rpm ]; then
+	mv *.gz ${HOME}/rpm/SOURCES/
+	rpmbuild -ba login-email.spec
+fi
