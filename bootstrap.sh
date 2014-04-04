3016d40f88207ca771f107afd32251cb4ffb60ba
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-04-03 19:00:46 -0500

    Build RPM package locally.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,11 @@ markdown2 README.md | tee README.html | lynx -dump -stdin >README
 autoreconf -fvim
 ./configure
 make dist
-if [ -d ${HOME}/rpm ]; then
-	mv *.gz ${HOME}/rpm/SOURCES/
-	rpmbuild -ba login-email.spec
-fi
+rm -rf RPM
+mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
+rpmbuild								\
+	-D"_topdir ${PWD}/RPM"						\
+	-D"_sourcedir ${PWD}"						\
+	-D"_specdir ${PWD}"						\
+	-ba								\
+	login-email.spec
