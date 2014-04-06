f1af46d81969a2d1095a3dd05001ff8edbc9d4ac
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-04-06 17:07:03 -0500

    Checkpoint

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,9 @@ autoreconf -fvim
 make dist
 rm -rf RPM
 mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
-rpmbuild								\
-	-D"_topdir ${PWD}/RPM"						\
-	-D"_sourcedir ${PWD}"						\
-	-D"_specdir ${PWD}"						\
-	-ba								\
+rpmbuild							\
+	-D"_topdir ${PWD}/RPM"					\
+	-D"_sourcedir ${PWD}"					\
+	-D"_specdir ${PWD}"					\
+	-ba							\
 	login-email.spec
