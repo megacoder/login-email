f4cca03d5253aa9ab970fb71d6072ea451cfd878
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-12-01 09:11:48 -0500

    Check for required RPM packages.

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -1,4 +1,22 @@
 #!/bin/zsh
+RPMS=(
+	automake
+	lynx
+	python-markdown2
+	rpm-build
+)
+STOP=
+for rpm in ${RPMS}; do
+	rpm -q --quiet "${rpm}"
+	if [[ ! $? ]]; then
+		echo "-- Need ${rpm}" >&2
+		STOP="yes"
+	fi
+done
+if [[ ! -z "${STOP}" ]]; then
+	echo "You need to get more RPM's." >&2
+	exit 1
+fi
 markdown2 README.md | tee README.html | lynx -dump -stdin >README
 autoreconf -fvim
 ./configure
